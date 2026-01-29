26a3b78fe964688e237cde12e0c4b0c19c199a21
rifqiammarramadhan <rifqiammarramadhan103@gmail.com>
2026-01-29 16:40:01 +0700

    update

---
+++ AssesmentTest/Views/Home/Home.razor.cs
@@ -0,0 +1,42 @@
+﻿using AssesmentTest.Views.Pages;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+
+namespace AssesmentTest.Views.Home
+{
+    public partial class Home
+    {
+        // IHttpClientFactory set using dependency injection 
+        [Inject]
+        public required IHttpClientFactory HttpClientFactory { get; set; }
+
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+
+        /* Add the data model, an array is expected as a response */
+        private IEnumerable<Products>? _products;
+
+        // Begin GET operation when the component is initialized
+        protected override async Task OnInitializedAsync()
+        {
+            // Create the HTTP client using the FruitAPI named factory
+            var httpClient = HttpClientFactory.CreateClient("FruitAPI");
+
+            // Perform the GET request and store the response. The parameter
+            // in GetAsync specifies the endpoint in the API 
+            using HttpResponseMessage response = await httpClient.GetAsync("/fruits");
+
+            // If the request is successful deserialize the results into the data model
+            if (response.IsSuccessStatusCode)
+            {
+                using var contentStream = await response.Content.ReadAsStreamAsync();
+                _products = await JsonSerializer.DeserializeAsync<IEnumerable<Products>>(contentStream);
+            }
+            else
+            {
+                // If the request is unsuccessful, log the error message
+                Console.WriteLine($"Failed to load fruit list. Status code: {response.StatusCode}");
+            }
+        }
+    }
+}
---
+++ AssesmentTest/Views/Pages/Add.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text;
+using System.Text.Json;
+
+namespace AssesmentTest.Views.Pages
+{
+    public partial class Add : ComponentBase
+    {
+        // IHttpClientFactory set using dependency injection 
+        [Inject]
+        public required IHttpClientFactory HttpClientFactory { get; set; }
+
+        // NavigationManager set using dependency injection
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+
+        // Add the data model and bind the form data to it
+        [SupplyParameterFromForm]
+        private Products? _products { get; set; }
+
+        protected override void OnInitialized() => _products ??= new();
+
+        // Begin POST operation code
+        private async Task Submit()
+        {
+            // Serialize the information to be added to the database
+            var jsonContent = new StringContent(JsonSerializer.Serialize(_products),
+                Encoding.UTF8,
+                "application/json");
+
+            // Create the HTTP client using the FruitAPI named factory
+            var httpClient = HttpClientFactory.CreateClient("FruitAPI");
+
+            // Execute the POST request and store the response. The response will contain the new record's ID
+            using HttpResponseMessage response = await httpClient.PostAsync("/fruits", jsonContent);
+
+            // Check if the operation was successful, and navigate to the home page if it was
+            if (response.IsSuccessStatusCode)
+            {
+                NavigationManager?.NavigateTo("/");
+            }
+            else
+            {
+                Console.WriteLine("Failed to add fruit. Status code: {response.StatusCode}");
+            }
+        }
+    }
+}
